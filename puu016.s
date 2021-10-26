392ce5877c4ef39ef7f7e8c81df400eab7e1541d
K-P Koljonen <kpkoljonen@gmail.com>
2021-10-26 19:59:21 +0300

    Adjust author info

--- puu016.s
+++ puu016.s
@@ -1222,96 +1222,6 @@ p_NOP macro
 	dc.l	$4e754e75
 	endm
 
-; * Player ids
-; * These are self contained, replayer code inside the module
-; pt_internal_start = 33
-; 	rsset	pt_internal_start
-; pt_prot		rs.b	1
-; pt_sid		rs.b	1
-; pt_delta2	rs.b	1
-; pt_musicass	rs.b	1
-; pt_fred		rs.b	1
-; pt_sidmon1	rs.b	1
-; pt_med		rs.b	1
-; pt_markii	rs.b	1
-; pt_mon		rs.b	1
-; pt_dw		rs.b	1
-; pt_hippel	rs.b	1
-; pt_mline	rs.b	1
-; pt_beathoven	rs.b	1
-; pt_delicustom	rs.b 	1
-
-; * These need a replayer from the group file
-; * These are ids into the file and must match playergroup0.s
-; pt_group_start = 49
-; 	rsset	pt_group_start		* Ulkoiset
-; pt_multi	rs.b	1		* PS3M (mod,ftm,mtm,s3m)
-; pt_tfmx		rs.b	1
-; pt_tfmx7	rs.b	1
-; pt_jamcracker	rs.b	1
-; pt_future10	rs.b	1
-; pt_future14	rs.b	1
-; pt_soundmon2	rs.b	1
-; pt_soundmon3	rs.b	1
-; pt_oktalyzer	rs.b	1
-; pt_player	rs.b	1
-; pt_hippelcoso	rs.b	1
-; pt_digibooster	rs.b	1
-; pt_thx		rs.b	1
-; pt_sample	rs.b	1
-; pt_aon		rs.b	1
-; pt_digiboosterpro rs.b	1
-; pt_pumatracker	rs.b 	1
-; pt_gamemusiccreator	rs.b 	 1
-; pt_digitalmugician 	rs.b  	1
-; pt_medley 	 	rs.b  	1
-; pt_futureplayer 	rs.b 	1
-; pt_bendaglish		rs.b 	1
-; pt_sidmon2		rs.b	1
-; pt_deltamusic1 		rs.b 	1
-; pt_soundfx		rs.b	1
-; pt_gluemon		rs.b	1
-; pt_pretracker		rs.b	1
-; pt_custommade		rs.b 	1
-; pt_sonicarranger	rs.b	1
-; pt_davelowe		rs.b	1
-; pt_startrekker		rs.b 	1
-
-; pt_eagle_start = 1000
-; 	rsset	pt_eagle_start
-; pt_synthesis		rs.b 	1
-; pt_syntracker		rs.b  	1
-; pt_robhubbard2 		rs.b 	1
-; pt_chiptracker		rs.b	1
-; pt_quartet		rs.b    1
-; pt_facethemusic		rs.b 	1	
-; pt_richardjoseph 	rs.b	1
-; pt_instereo1		rs.b    1
-; pt_instereo2       	rs.b    1
-; pt_jasonbrooke		rs.b	1
-; pt_earache		rs.b 	1
-; pt_krishatlelid		rs.b    1
-; pt_richardjoseph2 	rs.b	1
-; pt_hippel7		rs.b	1
-; pt_aprosys		rs.b	1
-; pt_hippelst		rs.b	1
-; pt_tcbtracker		rs.b 	1
-; pt_markcooksey		rs.b	1
-; pt_activisionpro	rs.b	1
-; pt_maxtrax		rs.b 	1
-; pt_wallybeben		rs.b 	1
-; pt_synthpack		rs.b    1
-; pt_jeroentel		rs.b 	1 
-; pt_robhubbard 		rs.b 	1
-; pt_sonix		rs.b 	1
-; pt_coredesign		rs.b    1
-; pt_quartetst	   	rs.b    1
-; pt_digitalmugician2	rs.b	1
-; pt_musicmaker4		rs.b	1
-; pt_musicmaker8		rs.b	1
-; pt_soundcontrol		rs.b    1
-; pt_stonetracker		rs.b	1
-
  if pt_prot<>33 
    fail This must be 33
  endc 
@@ -1319,10 +1229,8 @@ p_NOP macro
    fail This must be 49
  endc 
 
-
 * player group version
 xpl_versio	=	22
-
 
 *********************************************************************************
 *
@@ -29615,7 +29523,7 @@ p_musicassembler
 	dc.w pt_musicass				* type
 	dc	pf_cont!pf_stop!pf_volume!pf_ciakelaus
 	dc.b	"Music Assembler",0
-.a 	dc.b	"Oscar Giesen & Marco Swagerman",0
+.a 	dc.b	"Oscar Giesen, Marco Swagerman",0
  even
 
 .author
@@ -32831,7 +32739,7 @@ p_mline
 	dc.w pt_mline
 	dc	pf_cont!pf_stop!pf_volume!pf_song
 	dc.b	"MusiclineEditor",0
-.a 	dc.b	"Conny Cyreus, Christian Cyreus, Jimmy Fredriksson & John Carehag",0
+.a 	dc.b	"Conny Cyreus, Christian Cyreus, Jimmy Fredriksson, John Carehag",0
  even
 	
 .author
@@ -33469,16 +33377,11 @@ p_sample
 	p_NOP		* Taakse
 	jmp	.ahiup(pc)		* AHI Update
 	jmp id_sample(pc)
-	jmp	.author(pc)
+	p_NOP		* author
 	dc.w pt_sample
 	dc	pf_volume!pf_scope!pf_stop!pf_cont!pf_end!pf_ahi
 .name	dc.b	"                        ",0
-.a		dc.b	"Samples by K-P, MP3 by Stephane Tavenard",0
  even
-
-.author
-	pushpea	.a(pc),d0
-	rts
 
 	rsset	$20
 .s_init		rs.l	1
@@ -33602,7 +33505,7 @@ p_pumatracker
 	dc.w pt_pumatracker
 	dc	pf_stop!pf_cont!pf_ciakelaus
 	dc.b	"PumaTracker",0
-.a  dc.b "Jean-Charles Meyrignac, Pierre-Eric Loriaux, adapted by Flyspy/Agile",0
+.a  dc.b "Jean-Charles Meyrignac, Pierre-Eric Loriaux",0
  even
 
 .author
@@ -35055,7 +34958,7 @@ p_soundfx
 	dc.w pt_soundfx
 	dc	pf_stop!pf_cont!pf_ciakelaus!pf_volume!pf_poslen
 	dc.b	"SoundFX",0
-.a 	dc.b	"Christian Haller & Christian A. Webber",0
+.a 	dc.b	"Christian Haller, Christian A. Webber",0
  even
 
 .author
